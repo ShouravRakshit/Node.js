01fe82d922355a78257977dcb691723f33e6d923
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-09-03 19:25:36 -0600

    changed

--- Experiment MVC/index.js
+++ Experiment MVC/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import userRouter from "./routes/users.route.js";
+import cors from 'cors';
 
 const port = 3000;
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(userRouter)
+app.use(cors());
 
 app.use((req, res, next) =>{
     res.status(404).json({
