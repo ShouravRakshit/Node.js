fcd2c776fcb33a14899eef3d16c2bab6a281b3a7
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-09-05 23:38:00 -0600

    working with db api

---
+++ Rest Api with DB/config/config.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dev = {
+    index: {
+        port : process.env.PORT || 3000
+    },
+    db:{
+        url: process.env.DB_URL || "mongodb://localhost:27017/userDemoDB"
+    }
+}
+
+export default dev;
---
+++ Rest Api with DB/controller/user.controller.js
@@ -0,0 +1,32 @@
+const getAllUsers = (req, res) =>{
+    res.status(200).json({
+        message : "found all users"
+    })
+};
+
+const getOneUser = (req, res) =>{
+    res.status(200).json({
+        message : "get one user"
+    })
+}
+
+const createUser = (req, res) =>{
+    res.status(201).json({
+        message : "user is created"
+    })
+}
+
+const updateUser = (req, res) =>{
+    res.status(200).json({
+        message : "user is updated"
+    })
+}
+
+const deleteUser = (req, res) =>{
+    res.status(200).json({
+        message : "user is deleted"
+    })
+}
+
+
+export {getAllUsers, getOneUser, createUser, updateUser, deleteUser};
